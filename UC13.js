fb3d4dd18ba0e59eabbcb1e47cbf921f9c6e7b42
marcusnogueira <marcus.nogueira1@gmail.com>
2017-03-31 08:41:33 -0700

    Update UC13.js

--- UC13.js
+++ UC13.js
@@ -1,18 +1,18 @@
 function Cube (length, mass ){
-this.length = length
-this.mass = mass
-this.volume = function()
-volume = length * length * length;
-return volume;
+     this.length = length
+     this.mass = mass
+     this.volume = function()
+     volume = length * length * length;
+     return volume;
 }
-   this.density = function() {
+    this.density = function() {
         var density;
         density = mass / length * length * length;
         return density;
-    }
-    this.surfaceArea = function() {
+}
+   this.surfaceArea = function() {
         var surfaceArea;
         surfaceArea = length * length * 6;
         return surfaceArea;
-    }
+}
 };

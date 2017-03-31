3c2b01de0a18430b1acec8ff0e438a5597e37d42
marcusnogueira <marcus.nogueira1@gmail.com>
2017-03-31 08:32:10 -0700

    Update UC13.js
    Density and Surface Area done

--- UC13.js
+++ UC13.js
@@ -5,3 +5,14 @@ this.volume = function()
 volume = length * length * length;
 return volume;
 }
+   this.density = function() {
+        var density;
+        density = mass / length * length * length;
+        return density;
+    }
+    this.surfaceArea = function() {
+        var surfaceArea;
+        surfaceArea = length * length * 6;
+        return surfaceArea;
+    }
+};
